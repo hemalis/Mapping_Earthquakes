b28be4fab0d4b05a8024db241ca3075986ad1acb
Hemali Shah <hemshi88@gmail.com>
2022-09-06 01:12:25 -0700

    added new files

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -12,19 +12,19 @@ let map = L.map("mapid", {
   });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
 });
 
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 /* L.circle([34.0522, -118.2437], {
   radius: 100
-}).addTo(map); */
+}).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+streets.addTo(map); */
 
 /* L.circleMarker([34.0522, -118.2437], {
   radius: 300,
@@ -33,5 +33,26 @@ streets.addTo(map);
 }).addTo(map);
  */
 
-  
+// L.marker([34.0522, -118.2437]).addTo(map);
+
+/* {
+  location: [40.7128, -74.0059],
+  city: "New York City",
+  state: "NY",
+  population: 8398748
+} */
+
+let cityData = cities;
+
+cityData.forEach(ct => {
+    L.circleMarker(ct.location, {
+        radius: ct.population/100000,
+        color: "black",
+        fillColor: "#ffffa1"
+    })
+    .bindPopup("<h2>" + ct.city + ", " + ct.state + "</h2> <hr> <h3>Population " + ct.population.toLocaleString() + "</h3>")
+    .addTo(map)
+})
+streets.addTo(map);
+
 
